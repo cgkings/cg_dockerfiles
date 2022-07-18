77eb4141c3e25ce1ba231dfa7fa3d75364726c48
cgking <58586625+cgkings@users.noreply.github.com>
2022-07-18 13:55:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ mkdir -p /etc/qBittorrent/data/GeoIP
 #创建下载目录
 mkdir -p /downloads
 #下载GeoIP数据库
-curl -kLo /root/GeoLite2-Country.mmdb https://github.com/helloxz/qbittorrent/raw/main/GeoLite2-Country.mmdb
+curl -kLo /root/GeoLite2-Country.mmdb https://github.com/PrxyHunter/GeoLite2/releases/latest/download/GeoLite2-Country.mmdb
 
 chmod +x /usr/sbin/run.sh
+
